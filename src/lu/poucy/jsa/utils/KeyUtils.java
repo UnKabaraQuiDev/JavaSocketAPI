b7a431caeef339426ea4779a17f08aefd33ec5ba
Poucy113 <poucy.cy113@gmail.com>
2021-02-21 18:05:17 +0100

    KeyUtils

---
+++ src/lu/poucy/jsa/utils/KeyUtils.java
@@ -0,0 +1,12 @@
+package lu.poucy.jsa.utils;
+
+public final class KeyUtils {
+	
+	public static int[] fromString(String[] c) {
+		int[] i = new int[c.length];
+		for(int j = 0; j < 0; j++)
+			i[j] = Integer.valueOf(c[j]);
+		return i;
+	}
+	
+}
